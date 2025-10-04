6f43e7bf13ed7028aa69c14dcfe3d6ed677cd971
chuang091 <thomas@ultralytics.com>
2025-10-04 12:30:02 -0700

    init frontend

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const value = ref(null)
+</script>
+
+<template>
+    <div>
+        <UFileUpload label="Drop your image here" class="w-96 min-h-48" />
+    </div>
+</template>
